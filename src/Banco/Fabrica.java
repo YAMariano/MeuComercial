78909a22c43013e79d6a97ffaa197958ce7fbd14
aluno <aluno@LAB01MC01>
2018-11-06 21:33:39 -0200

    create Fabrica

--- src/Banco/Fabrica.java
+++ src/Banco/Fabrica.java
@@ -1,5 +1,17 @@
 package Banco;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Fabrica {
 
+	private static EntityManagerFactory fabrica;
+	
+	public static EntityManagerFactory get() {
+		
+		if(fabrica == null) {
+			fabrica = Persistence.createEntityManagerFactory("meu_comercial");
+		}
+		return fabrica;
+	}
 }
